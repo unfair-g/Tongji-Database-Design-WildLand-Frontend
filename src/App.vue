6acd27be1fbdbb831a4efac9c15bddc385aaab41
zitroneeee <362733473@qq.com>
2024-07-13 01:00:46 +0800

    经验资讯1.0
    element plus引入存在问题
    排版待优化

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <HomePage />
+  <div id="app">
+    <your-component />
+  </div>
 </template>
 
 <script>
-//import HelloWorld from './pages/HelloWorld.vue'
-import HomePage from './pages/HomePage.vue'
+import YourComponent from './components/flashMessage.vue'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld,
-    HomePage
+    YourComponent
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  width: 1920px;
+  height: 1080px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
