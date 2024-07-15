ffd8cc32ebcda187d06def158fcbd9248347bd7e
L1KEAB0T <wjy1370858716@gmail.com>
2024-07-15 11:19:23 +0800

    新添加了一个帖子审核中的帖子详情，新增了router
    点击左侧帖子审核出现帖子审核，点击详情可以看帖子具体内容，按确认或者点击×返回

---
+++ L1KEAB0T/index.js
@@ -0,0 +1,79 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HelloWorld from '../pages/HelloWorld.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'welcome',
+        component: HelloWorld
+    },
+    {
+        path: '/administrator',
+        component: () => import('../pages/AdministratorPage.vue'),
+        children:[
+            {
+                path: 'postaudit',
+                name: 'PostAudit',
+                component: () => import('../views/PostAudit.vue')
+            },
+            {
+                path: 'postdetail',
+                name: 'PostDetail',
+                component: () => import('../views/PostDetail.vue')
+            }
+        ]
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: () => import('../pages/HomePage.vue'),
+        children: [
+            {
+                path: "",
+                component: () => import('../views/HomeView.vue')
+            },
+            {
+                path: "product",
+                component: () => import('../views/ItemView.vue')
+            },
+            {
+                path: "forum",
+                component: () => import('../views/ForumView.vue')
+            },
+            {
+                path: "campground",
+                component: () => import('../views/CampgroundView.vue')
+            },
+            {
+                path: "userspace",
+                component: () => import('../views/UserSpaceView.vue')
+            },
+            {
+                path: "flash",
+                component: () => import('../views/FlashView.vue')
+            }
+        ]
+    },
+    {
+        path: '/enter',
+        component: () => import('../views/EnterView.vue'),
+        children: [
+            {
+                path: "login",
+                component: () => import('../components/LogIn.vue')
+            },
+            {
+                path: "enrollment",
+                component: () => import('../components/UserEnroll.vue')
+            }
+        ]
+    }
+
+]
+
+const router = createRouter({
+    history: createWebHashHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
