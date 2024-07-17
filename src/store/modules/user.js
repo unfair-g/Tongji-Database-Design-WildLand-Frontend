fc4f93bef235574e43cfe418b51e8ee85c28b1a9
unfair-g <2814315943@qq.com>
2024-07-17 17:58:20 +0800

    vuex优化

---
+++ src/store/modules/user.js
@@ -0,0 +1,17 @@
+export default {
+    state(){
+        return {
+            user_id: 2004090642,
+            user_name: 'unfair',
+            password: '123456',
+            email: '2814315943@qq.com',
+            phone_number: 18247855400,
+            gender: '女',
+            birthday: '2004-9-6',
+            personal_signature: 'lalalalalala',
+            location: '上海',
+            points: 50,
+            outdoor_master_title: true
+        }
+    }
+}
