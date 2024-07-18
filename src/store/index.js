721b5143210e4054fadb42f819e41e9efcd8ace8
unfair-g <2814315943@qq.com>
2024-07-19 00:05:39 +0800

    merge经验资讯

--- src/store/index.js
+++ src/store/index.js
@@ -8,30 +8,30 @@ export default createStore({
   state: {
     tags: [
       {
-        title:'Tag1',
-      image:require('@/assets/flash-1.png'),
+        title: 'Tag1',
+        image: require('@/assets/flash-1.png'),
       },
       {
-        title:'Tag1',
-        image:require('@/assets/flash-1.png'),
+        title: 'Tag1',
+        image: require('@/assets/flash-1.png'),
       },
       {
-        title:'Tag1',
-        image:require('@/assets/flash-1.png'),
+        title: 'Tag1',
+        image: require('@/assets/flash-1.png'),
       }], // 假设的tags列表  
     selectedTags: [] // 存储选中的tags  
   },
   getters: {
   },
   mutations: {
-    toggleTag(state, tag) {  
-      const index = state.selectedTags.indexOf(tag);  
-      if (index > -1) {  
-        state.selectedTags.splice(index, 1);  
-      } else {  
-        state.selectedTags.push(tag);  
-      }  
-    }  
+    toggleTag(state, tag) {
+      const index = state.selectedTags.indexOf(tag);
+      if (index > -1) {
+        state.selectedTags.splice(index, 1);
+      } else {
+        state.selectedTags.push(tag);
+      }
+    }
   },
   actions: {
   },
