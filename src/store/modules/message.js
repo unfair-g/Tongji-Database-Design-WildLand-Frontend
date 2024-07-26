bf028fcc7a79618a380f43d3948757724919f1ed
unfair-g <148096118+unfair-g@users.noreply.github.com>
2024-07-26 17:02:12 +0800

    merge Wsy branch
    * 经验资讯1.0
    
    element plus引入存在问题
    排版待优化
    
    * 加上导航栏
    
    * 将模块放入组件方便复用
    
    * 增加详情页
    
    稍微修改其他排版
    
    * 微调格式
    
    * 微调格式
    
    * 加入管理员部分界面
    
    部分待完善
    
    * 使用store
    
    * 增加部分页面
    
    修改了部分遗留bug，删除了资讯的评论功能
    
    * bug修复+微调
    
    消息通知界面筛选没做完，待修改

--- src/store/modules/message.js
+++ src/store/modules/message.js
@@ -3,6 +3,7 @@ export default{
         messages: [
             {  
                 id: 1,  
+                type:'follow',
                 username: '张三',  
                 timestamp: '1小时前',  
                 content: '这是一条测试消息。',  
@@ -11,11 +12,14 @@ export default{
               {  
                 id: 2,  
                 username: '张三',  
+                type:'activity',
                 timestamp: '1小时前',  
                 content: '这是一条测试消息。',  
                 avatarUrl: 'https://gd-hbimg.huaban.com/7968f776596196a8061e9ee0ee51c0606d785fc42400b-9aWWPH_fw236' // 假设的头像URL  
               },  
-              { id: 3, username: '李四', timestamp: '刚刚', content: '这是新加载的一条消息。',avatarUrl: 'https://tse4-mm.cn.bing.net/th/id/OIP-C.uMf5AX3a6yYpIhpEkyDxiQAAAA?rs=1&pid=ImgDetMain' },  
+              { id: 3, username: '李四',
+                type:'tip-off',
+                timestamp: '刚刚', content: '这是新加载的一条消息。',avatarUrl: 'https://tse4-mm.cn.bing.net/th/id/OIP-C.uMf5AX3a6yYpIhpEkyDxiQAAAA?rs=1&pid=ImgDetMain' },  
             ], // 假设的tags列表  
           selectedTags: [] // 存储选中的tags  
     },
