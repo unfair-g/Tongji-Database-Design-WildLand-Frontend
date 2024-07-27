2ebccc6e15df478934c3ff3559fa7447f1a1650d
unfair-g <2814315943@qq.com>
2024-07-28 00:30:41 +0800

    个人空间收藏完善

--- src/components/UserFavourites.vue
+++ src/components/UserFavourites.vue
@@ -1,7 +1,7 @@
 <template>
 <div style="display: flex;padding-left:2%;padding-top:1% ">
     <h3 style="font-size:55px">我的收藏夹</h3>
-    <el-tabs v-model="componentTag"  class="favourites" stretch="true">
+    <el-tabs v-model="componentTab"  class="favourites" stretch="true">
         <el-tab-pane label="营地" name="camp" width="10px">
         </el-tab-pane>
         <el-tab-pane label="户外用品" name="product">
@@ -13,21 +13,58 @@
     </el-tabs>
 </div>
 <div style="margin:2%;">
-<component :is="componentTag"></component>
+    <el-row :gutter="25" v-if="componentTab==='camp'">
+        <el-col :span="8" v-for="camp in starcamp" :key="camp.id">
+            <el-card  style="margin-bottom:8%">
+                <img :src="camp.image" style="width:100%"/>
+                <template #footer>
+                    <h3>{{ camp.campground_name }}</h3>
+                    <div style="margin-top:3%">{{ camp.introduction }}</div>
+                </template>
+            </el-card>
+        </el-col>
+  </el-row>
+  <el-row :gutter="25" v-else-if="componentTab==='product'">
+        <el-col :span="8" v-for="product in starproduct" :key="product.product_id">
+            <el-card  style="margin-bottom:8%">
+                <img :src="product.product_image" style="width:100%;height:300px"/>
+                <template #footer>
+                    <h3>{{ product.product_name }}</h3>
+                    <div style="margin-top:3%">{{ product.introduction }}</div>
+                </template>
+            </el-card>
+        </el-col>
+  </el-row>
+  <el-row :gutter="25" v-else-if="componentTab==='flash'">
+        <el-col :span="8" v-for="flash in starflash" :key="flash.flash_id">
+            <el-card  style="margin-bottom:8%">
+                <img :src="product.product_image" style="width:100%;height:300px"/>
+                <template #footer>
+                    <h3>{{ product.product_name }}</h3>
+                    <div style="margin-top:3%">{{ product.introduction }}</div>
+                </template>
+            </el-card>
+        </el-col>
+  </el-row>
 </div>
 </template>
 
 <script>
-import camp from './StarCamp.vue'
-
 export default {
-    components: {
-        camp
+     computed: {
+        starcamp() {
+            return this.$store.state.camp.camps;
+        },
+        starproduct() {
+            return this.$store.state.product.products;
+        },
+        starflash() {
+            return this.$store.state.flash.flashes;
+        }
     },
     data() {
         return {
-            activeTab: '营地',
-            componentTag:'camp'  
+            componentTab:'camp'  
         }
     }
 }
