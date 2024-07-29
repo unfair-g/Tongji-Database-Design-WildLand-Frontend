4dd9692e63d0cb211f8bf9165506ef0ff059e5a8
unfair-g <148096118+unfair-g@users.noreply.github.com>
2024-07-29 23:39:09 +0800

    merge Fby branch
    * 帖子详情页面初版提交
    
    修改了Article.vue 、index.js  、post.js，实现了动态存取帖子的信息，显示forum主页的Article，创建了postDetailView.vue存放帖子详情页面，实现了页面之间的跳转，帖子详情页面除评论点击icon图标显示回复框外基本均已实现。
    
    * 更新main分支内容
    
    * 删除了fbyreadme
    
    * 拆分及优化
    
    将评论输入框、评论显示拆分成组件，并引入帖子详情页面。
    
    搭起贴子发布的框架（包括边框、main、跳转、参数传入）；
    
    对sidebar进行修改（实现了禁用时无法发帖弹窗和未禁用时选择发帖类别并跳转的逻辑）；
    
    对帖子详情的bug进行修改，实现了评论后可添加入comment队列，对某一comment点击评论按钮可弹出输入框并输入评论；但仍存在bug: 1、输入的评可以加入reply队列但是无法显示评论内容，其他内容均显示；2、reply的评论按钮也可点击输入评论，但是不会被添加到reply的reply队列(post.js内未定义)，后续我将进行修改
    
    * 修改复用
    
    修改了Article使不同帖子类型对应显示不同的卡片
    
    * merge后版本的部分问题修复
    
    解决了两个报错，但仍然存在论坛主页帖子显示问题
    
    * 加入招募贴详情页
    
    实现了与分享贴不同的招募贴详情页与首页展示
    
    * 发布页初步实现
    
    发布页相关：
       完成了发布页-分享贴、招募贴的初步实现，还缺少地图选点、图片加载后预览等功能的实现，后续仍需完善。
      添加了SharepostPublish.vue   RecruitPostPublish.vue   修改了PublishAPost.vue和postDetailView.vue和PublishDetail.vue等内容
    
    论坛首页merge：
       将闲置贴的首页展示、跳转的代码拆分融入Article.vue中，更新main.vue以实现这部分功能
    
    包的安装：
        同组成员的axios已同步安装，由于地图的需求，我安装了vue-amap，输入指令npm install vue-ampa --save

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -65,7 +65,50 @@
       </el-card>
     </div>
   </div>
+  <div v-else-if="view === 'lease'">
+    <div v-for="ldleitemspost in ldleitemsposts" :key="ldleitemspost.post_id" justify="center" >
+      <el-card class="post-item" @click="goToPostDetail(ldleitemspost)">
+        <template #header>
+         <div class="post-header">
+            <img :src="ldleitemspost.avatar" alt="avatar" class="avatar">
+            <div class="post-details">
+              <div class="post-info">
+                <span class="username">{{ ldleitemspost.username }}</span>
+                <span class="time">{{ ldleitemspost.time }}</span>
+              </div>
+              <div class="post-stats">
+                <span class="stat-item"><el-icon><View /></el-icon>{{ ldleitemspost.views }}</span>
+                <span class="stat-item" @click="toggleLike">
+                  <i :class="{'iconfont': true, 'like-icon': true, 'icon-dianzan': !isLiked, 'icon-dianzanxuanzhong': isLiked}"></i>{{ ldleitemspost.likes }}
+                </span>
+                <span class="stat-item"><el-icon><ChatLineSquare /></el-icon> {{ ldleitemspost.comments }}</span>
+                <span class="stat-item" @click="toggleStar">
+                  <el-icon v-if="!isStarred"><Star /></el-icon>
+                  <el-icon v-else><StarFilled /></el-icon>
+                </span>
+              </div>
+            </div>
+          </div>
+        </template>
 
+        <div class="post-content">
+          <img :src="ldleitemspost.item_image" class="image" alt="order image">
+          <div style="padding: 14px;flex:1;">
+            <span>{{ ldleitemspost.item_name}}</span>
+            <div><span>商品简介: {{ ldleitemspost.item_summary}}</span></div>
+            <div><span>商品新旧程度：{{ ldleitemspost.condition}}</span></div>
+            <div class="bottom clearfix">
+              <span class="price">¥{{ ldleitemspost.price }}</span>
+              <el-button type="text" class="button" @click="goToPostDetail(ldleitemspost)">查看详情</el-button>
+            </div>
+          </div>
+        </div>
+      </el-card>
+    </div>
+  </div>
+  <div v-else>
+    加载中……
+  </div>
 </template>
 
 <script>
@@ -84,12 +127,20 @@ export default {
     },
     recruitposts() {
       return this.$store.state.post.recruitmentposts; 
+    },
+    ldleitemsposts() {
+      return this.$store.state.post.ldleitemsposts
     }
+
   },
   methods: {
     goToPostDetail(post) {
       const postID = post.post_id;
-      this.$router.push({ path: `/home/forum/post/${this.view}/${postID}` });
+      if (this.view === 'lease') {
+        this.$router.push({ path: `/home/forum/lease/${postID}` });
+      } else {
+        this.$router.push({ path: `/home/forum/post/${this.view}/${postID}` });
+      }      
     },
     toggleLike(post) {
       post.isLiked = !post.isLiked;
@@ -156,5 +207,26 @@ i {
 .post-title:hover,
 .post-text:hover {
   text-decoration: underline; /* 添加下划线 */
+}
+.product-card {
+  width: 100%; /* 固定宽度 */
+  height:30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.image {
+  width: 200px; /* 固定图片宽度 */
+  height: 200px; /* 固定图片高度 */
+  object-fit: cover;
+  margin-right: 60px;
+}
+.price {
+  color: #ff4949;
+  font-size: 18px;
+}
+.button {
+  float: right;
+  padding: 0;
 }
 </style>
